1c3d899639a516b572534f9785342e8aec9738c2
ramsreddyam <ramsreddyam@gmail.com>
2023-10-22 12:58:08 +0530

    commit

---
+++ int.py
@@ -0,0 +1,11 @@
+num1 = 10
+num2 = 5
+
+result1 = num1 // num2
+print("division:", result1)
+
+result2 = num1 * num2
+print("multiplication:", result2)
+
+result3 = abs(-6)
+print("Absolute value:", result3)
